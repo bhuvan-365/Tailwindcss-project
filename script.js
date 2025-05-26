afdce74b22ed366db7a2077b0519bbc2fb7469b3
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-05-26 07:45:48 +0545

    added images

---
+++ script.js
@@ -0,0 +1,9 @@
+let imgbtn = document.querySelector(".imgbtn")
+let uploadcaption = document.querySelector(".uploadcaption")
+let uploadImg = document.querySelector(".uploadImg")
+let postContent = document.querySelector(".postContent")    
+
+postContent.addEventListener("click", () => {
+
+    console.log("clicked")
+})
