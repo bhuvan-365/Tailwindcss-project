fa427323e3c3e6d2152319014e1c400d43b5ec9d
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-06-11 18:03:16 +0545

    file fixed

--- script.js
+++
@@ -1,9 +0,0 @@
-let imgbtn = document.querySelector(".imgbtn")
-let uploadcaption = document.querySelector(".uploadcaption")
-let uploadImg = document.querySelector(".uploadImg")
-let postContent = document.querySelector(".postContent")    
-
-postContent.addEventListener("click", () => {
-
-    console.log("clicked")
-})
